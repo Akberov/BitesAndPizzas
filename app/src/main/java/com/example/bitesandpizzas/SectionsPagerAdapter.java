ab3f7b8035b41d485a43f688578b9a1d0f00e307
Renat Akberov <rentatnt@gmail.com>
2020-03-24 23:14:11 +0300

    after 12'th chapter

---
+++ app/src/main/java/com/example/bitesandpizzas/SectionsPagerAdapter.java
@@ -0,0 +1,7 @@
+package com.example.bitesandpizzas;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
